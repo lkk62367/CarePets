8dc3056bba5de5510b98ee079fdfae93e493075a
William <lkk623656@gmail.com>
2016-12-20 18:16:35 +0800

    first commit

---
+++ app/src/main/java/will/tw/carepets/ItemObject.java
@@ -0,0 +1,42 @@
+package will.tw.carepets;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ItemObject {
+    @SerializedName("_id")
+    private String id;
+    @SerializedName("Name")
+    private String name;
+    @SerializedName("Sex")
+    private String sex;
+    @SerializedName("Type")
+    private String type;
+    @SerializedName("Build")
+    private String build;
+    @SerializedName("ImageName")
+    private String imagename;
+
+    public ItemObject(String name, String sex, String type, String imagename) {
+        this.name = name;
+        this.sex = sex;
+        this.type = type;
+        this.imagename = imagename;
+    }
+
+    public String getSongTitle() {
+        return name;
+    }
+
+    public String getSongYear() {
+        return sex;
+    }
+
+    public String getSongAuthor() {
+        return type;
+    }
+
+    public String getImagename() {
+        return imagename;
+    }
+
+}
---
+++ app/src/main/java/will/tw/carepets/RecyclerViewAdapter.java
@@ -0,0 +1,47 @@
+package will.tw.carepets;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolders> {
+
+    private List<ItemObject> itemList;
+    private Context context;
+
+    public RecyclerViewAdapter(Context context, List<ItemObject> itemList) {
+        this.itemList = itemList;
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerViewHolders onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_pet, null);
+        RecyclerViewHolders rcv = new RecyclerViewHolders(layoutView);
+        return rcv;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolders holder, int position) {
+        Picasso.with(context)
+                .load(itemList.get(position).getImagename())
+                .resize(150, 150)
+                .into(holder.petimage);
+        holder.songTitle.setText("名稱: " + itemList.get(position).getSongTitle());
+        holder.songYear.setText("性別: " + itemList.get(position).getSongYear());
+        holder.songAuthor.setText("種類: " + itemList.get(position).getSongAuthor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.itemList.size();
+    }
+}
